f908ea0c89ae2db506fea56fca50b7362734d97f
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-30 14:47:18 +0530

    Update oi.c

--- oi.c
+++ oi.c
@@ -1,1 +1,15 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<=4;i++)
+{
+printf("\n hello");
+}
+getch();
+}
