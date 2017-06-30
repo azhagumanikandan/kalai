67c0ae96779ad10f3b5af64e78cc3b130f090f42
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-30 13:33:54 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter the number abc");
+scanf("%d%d%d",&a,&b,&c);
+if(a<b)
+{
+printf("\n the a is greater");
+}
+else if(a<c)
+{
+printf("\n the b is greater");
+}
+else
+{
+printf("\n the c is greater");
+}
+getch();
+}
