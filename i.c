783f16636eee696fa66719ce50e991c1917bfb38
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-07-01 13:54:55 +0530

    Update i.c

--- i.c
+++ i.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long long n;
+int count=0;
+clrscr();
+printf("\n enter the integer");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=4;
+count++;
+printf("\n no of digits %d",count);
+}
+getch();
+}
 
