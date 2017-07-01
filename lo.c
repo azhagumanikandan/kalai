34f651af27be459520e29ba64eab9d9b6b5f2e2a
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-07-01 11:26:16 +0530

    Update lo.c

--- lo.c
+++ lo.c
@@ -1,1 +1,16 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,num,sum=0;
+clrscr();
+printf("\n enter an natural number");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("\n sum=%d",sum);
+getch();
+}
